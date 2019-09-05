653e91cf7144bdb8868ff776b46c6275a9a5bd40
Emad Salehi <emad.s1178@yahoo.com>
2019-09-05 15:08:29 +0430

    Project Initialized With Test On Place Sample And Simple Interface.

---
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/Transformable.java
@@ -0,0 +1,34 @@
+package com.google.ar.sceneform.samples.hellosceneform;
+
+import com.google.ar.sceneform.ux.BaseTransformableNode;
+import com.google.ar.sceneform.ux.RotationController;
+import com.google.ar.sceneform.ux.TransformationSystem;
+import com.google.ar.sceneform.ux.TranslationController;
+
+public class Transformable extends BaseTransformableNode {
+    private final TranslationController translationController;
+    private final RotationController rotationController;
+
+    @SuppressWarnings("initialization") // Suppress @UnderInitialization warning.
+    public Transformable(TransformationSystem transformationSystem) {
+        super(transformationSystem);
+
+        translationController =
+                new TranslationController(this, transformationSystem.getDragRecognizer());
+        addTransformationController(translationController);
+
+        rotationController = new RotationController(this, transformationSystem.getTwistRecognizer());
+        addTransformationController(rotationController);
+
+    }
+
+    /** Returns the controller that translates this node using a drag gesture. */
+    public TranslationController getTranslationController() {
+        return translationController;
+    }
+
+    /** Returns the controller that rotates this node using a twist gesture. */
+    public RotationController getRotationController() {
+        return rotationController;
+    }
+}
