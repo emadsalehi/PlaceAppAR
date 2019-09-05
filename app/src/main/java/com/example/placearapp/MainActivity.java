bbeaee14daa972736964809500cfdb8f790ea316
Emad Salehi <emad.s1178@yahoo.com>
2019-09-05 21:29:34 +0430

    LoginActivity And activity_login.xml Modified.

--- app/src/main/java/com/example/placearapp/MainActivity.java
+++ app/src/main/java/com/example/placearapp/MainActivity.java
@@ -1,6 +1,31 @@
 package com.example.placearapp;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinButton, loginButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinButton = findViewById(R.id.main_join_button);
+        loginButton = findViewById(R.id.main_login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
