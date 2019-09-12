49c1d19a146d2ac1072b7f80bb2babfb8c47e073
Emad Salehi <emad.s1178@yahoo.com>
2019-09-12 18:13:30 +0430

    application added.

---
+++ app/src/main/java/com/example/placearapp/application/MainApplication.java
@@ -0,0 +1,19 @@
+package com.example.placearapp.application;
+
+import android.app.Application;
+
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        Parse.initialize(new Parse.Configuration.Builder(this)
+//                .applicationId("lQO1mvXj0LVdNHMkcI7FiZVtdxONCX5hYlIUThVN")
+//                .clientKey("cJLF9x34WujHPMov7QpHmj7mlv5NWvEW8p1kSLMg")
+//                .server("https://parseapi.back4app.com")
+//                .build()
+//        );
+    }
+}
